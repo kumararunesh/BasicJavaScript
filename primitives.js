c04d1b9adbc6a1e1974946dafceb56f1d41e0474
kumararunesh <kumararunesh211994@gmail.com>
2025-05-24 03:32:38 +0530

    commit 3

--- primitives.js
+++ primitives.js
@@ -3,3 +3,21 @@ console.log(typeof balance );
 let anotherBalance = new Number(123);
 console.log(anotherBalance.valueOf());
 console.log(typeof anotherBalance);
+
+let firstname =undefined;
+console.log(firstname); 
+
+let myString = "hello";
+let one = "World";
+let two =myString+ one;
+console.log(two);
+console.log(`Hello ${2+2}`);
+
+// let sm1 = Symbol();
+// let sm2 = Symbol();
+// console.log(sm1==sm2);
+
+
+let sm1 = Symbol("Hitesh");
+let sm2 = Symbol("Hitesh");
+console.log(sm1==sm2);
