4ebb33af7bfe2098f4a0a367c293941f98fc2e5f
kumararunesh <kumararunesh211994@gmail.com>
2025-05-24 03:49:00 +0530

    commit 4

---
+++ nonPrimitives.js
@@ -0,0 +1,20 @@
+let userName ={name:"Arunesh",isLoggedIn :true};
+
+console.log(userName);
+console.log(typeof userName);
+console.log(userName.name);
+userName.name = "Yuyu";
+console.log(userName);
+console.log(userName.name);
+
+let user ={"first name":"Arunesh",isLoggedIn :true};
+console.log(user);
+console.log(user["first name"]);
+
+let today = new Date();
+console.log(today.getDay());
+
+let heroes = ["Mr a","b",78];
+console.log(heroes[2]);
+
+ 
